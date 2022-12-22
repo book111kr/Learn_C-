e13d8c4285460b4010893ede8c61d81d3f74e835
book111kr <113752736+book111kr@users.noreply.github.com>
2022-12-22 12:00:29 +0900

    Create Hello_World.cpp

---
+++ code/Hello_World.cpp
@@ -0,0 +1,8 @@
+  #include <iostream>   // C++ 언어는 프로그램에 필요하거나 유용한 정보를 포함하는 여러 헤더를 정의합니다.
+  using namespace std;  // 라인 using namespace std; std 네임스페이스를 사용하도록 컴파일러에 지시합니다.
+  
+  // main()은 프로그램 실행이 시작되는 곳입니다. : '//'는 C++에서 사용할 수 있는 한 줄 주석입니다.
+  int main() {
+      cout << "Hello World";  // cout << "Hello World"; "Hello World" 메시지가 화면에 표시되도록 합니다
+      return 0;               // 다음 줄은 0을 반환합니다. main( ) 함수를 종료하고 호출 프로세스에 값 0을 반환하도록 합니다.
+  }
